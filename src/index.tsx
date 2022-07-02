b2a1e0efa3e3efc35b09142dbe3f0b95ff3bd11e
Ilya Pasternak <pasterzoom@gmail.com>
2022-07-03 01:26:59 +0300

    update index for react 18v

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {App} from './App';
-import {BrowserRouter} from 'react-router-dom';
-import {store} from './redux/reduxStore';
-import {Provider} from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import {App} from './App'
+import {BrowserRouter} from 'react-router-dom'
+import {store} from './redux/reduxStore'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+)
+
+root.render(
     <BrowserRouter>
         <Provider store={store}>
-                <App/>
+            <App/>
         </Provider>
     </BrowserRouter>,
-    document.getElementById('root')
-);
+)
+reportWebVitals()
